0dcffe75d62179637ef1f8d912d515add5b0a83c
AlexRz <rodriguez.alexandre067@gmail.com>
2023-06-21 17:35:15 +0200

    added $directory that stores namespaces for entities in src/entity

--- Core/ORM/ObjectRelationMapping.php
+++ Core/ORM/ObjectRelationMapping.php
@@ -2,7 +2,26 @@
 
 namespace Core\ORM;
 
+use App\Entity;
+
 class ObjectRelationMapping
 {
 
+    public static array $entityObjects;
+
+
+    /**
+     * @return array
+     * @todo ORM property
+     */
+    public static function entityReader(): array
+    {
+        $directory = scandir(__DIR__ . '/../../src/Entity');
+        for ($i = 2; $i < count($directory); $i++) {
+            var_dump($directory[$i]);
+            str_replace("/", "\\", $directory[$i]);
+        }
+        return $directory;
+
+    }
 }
