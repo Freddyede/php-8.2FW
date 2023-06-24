90f371044efbb1f01a31c79ec762effc906e6ad5
Franck <41587017+Freddyede@users.noreply.github.com>
2023-06-24 20:52:45 +0200

    Added Modules to ObjectRelationMappingModules::getPdo()
    Signed-off-by: Franck <41587017+Freddyede@users.noreply.github.com>

---
+++ Core/Modules/ObjectRelationMappingModules.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Core\Modules;
+
+use PDO;
+
+class ObjectRelationMappingModules
+{
+    private static array $multipleDatabases = [];
+
+    public static function getPdo(bool $activatedArray, ?array $multipleDatabases = NULL): array|null|PDO
+    {
+
+        require __DIR__ . '/../../bin/init/databases.php';
+        if ($activatedArray) {
+            for ($i = 0; $i < count($multipleDatabases); $i++) {
+                (
+                    !empty($multipleDatabases[$i]['username']) &&
+                    !empty($multipleDatabases[$i]['password']) &&
+                    !empty($multipleDatabases[$i]['databases'])
+                ) ? $multipleDatabases[$i] = new PDO('mysql:host=localhost;dbname=' . $multipleDatabases[$i]['databases'], $multipleDatabases[$i]['username'], $multipleDatabases[$i]['password']) : NULL;
+            }
+
+            return $multipleDatabases;
+        }
+        return (
+            !empty(DATABASES['username']) &&
+            !empty(DATABASES['password']) &&
+            !empty(DATABASES['databases'])
+        ) ? new PDO('mysql:host=localhost;dbname=' . DATABASES['databases'], DATABASES['username'], DATABASES['password']) : NULL;
+    }
+}
+
+class ObjectRelationMapping
+{
+    public static function simpleConnection(): PDO|array|null
+    {
+        return ObjectRelationMappingModules::getPdo(false);
+    }
+
+    public static function multipleConnection(): PDO|array|null
+    {
+        return ObjectRelationMappingModules::getPdo(true);
+    }
+}
